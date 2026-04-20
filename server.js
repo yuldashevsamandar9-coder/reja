b12cdacced3317845f5136bce5608a3a56ec0ba4
yuldashevsamandar9-coder <yuldashevsamandar9@gmail.com>
2026-04-20 12:51:25 +0900

    BRR:building ejs web server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const res = require("express/lib/response");
 
 // 1 Starting code
 app.use(express.static("public"));
@@ -15,13 +16,19 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
-
-app.get("/hello", function (req, res) {
-  res.end(`<h1> HELLO WORLD by Sam Developer</h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "succes " });
 });
-app.get("/gift", function (req, res) {
-  res.end(`Sovg'alar bulimiga xush kelibsiz`);
+app.get("/", function (req, res) {
+  res.render("harid");
 });
+// app.get("/hello", function (req, res) {
+//   res.end(`<h1> HELLO WORLD by Sam Developer</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//   res.end(`Sovg'alar bulimiga xush kelibsiz`);
+// });
 
 const server = http.createServer(app);
 let PORT = 491;
