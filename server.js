454df9b3dc1d9d344886625d52209d5e1e99be9d
yuldashevsamandar9-coder <yuldashevsamandar9@gmail.com>
2026-04-22 16:16:40 +0900

    BRR: publishing in portfolio

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const res = require("express/lib/response");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Starting code
 app.use(express.static("public"));
@@ -20,6 +30,11 @@ app.post("/create-item", (req, res) => {
   console.log(req.body);
   res.json({ test: "succes " });
 });
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
 app.get("/", function (req, res) {
   res.render("harid");
 });
