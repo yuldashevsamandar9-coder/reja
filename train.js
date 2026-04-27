2eb2774dc3b776603fc16b331d1032205c2d771f
yuldashevsamandar9-coder <yuldashevsamandar9@gmail.com>
2026-04-27 13:06:34 +0900

    feat: Task B bajarildi developda

--- train.js
+++ train.js
@@ -25,6 +25,55 @@
 // console.log(task2);
 
 /* Task tugadi     */
+
+/*  TAsk B 
+B-TASK
+
+Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin. MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+@MITASK
+*/
+// function countDigits(string) {
+// //   let count = 0;
+// //   for ( let dbs of string) {
+// //     if (dbs >= '0' && dbs <= '9'){
+// //       count ++;
+// //     }
+// //   }
+// //   return count;
+// // }
+// console.log(countDigits("absd23gsj5679"));
+
+// function countDigits(str) {
+//   let count = 0;
+//   for (let dbs of str) {
+//     if (dbs >= "0" && dbs <= "9") {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(countDigits("absd12hjs459"));
+
+// function count1(string) {
+//   let count2 = 0;
+//   for (let abs of string) {
+//     if (abs >= "0" && abs <= "9") {
+//       count2++;
+//     }
+//   }
+//   return count2;
+// }
+// console.log(count1("aabsd23gsj5679"));
+
+/* Task B tugadi */
+
+// function greeting(asdfgkh33452nlxfk) {
+//   let count = 0;
+//   const task1 = greeting.filter((ele, index) => {
+//     return ele !== null ? ele : 1;
+//   });
+// }
 // // console.log("train task ishga tushdi");
 // // // Callback function //
 
