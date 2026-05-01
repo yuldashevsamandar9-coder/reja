97e39dcb74d342e29c226b9c2b0f6b1abab70737
yuldashevsamandar9-coder <yuldashevsamandar9@gmail.com>
2026-05-01 10:23:28 +0900

    Feat: C Task bajarildi

--- train.js
+++ train.js
@@ -36,9 +36,38 @@ Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda
 //     if (abs >= "0" && abs <= "9") {
 //       count2++;
 //     }
-//   }
-//   return count2;
-// }
-// console.log(count1("aabsd23gsj5679"));
+// //   }
+// //   return count2;
+// // }
+// // console.log(count1("aabsd23gsj5679"));
 
-/* Task B tugadi */
+// /* Task B tugadi */
+
+// /* Task C start.
+// C-TASK
+
+// Shunday function tuzing, u 2ta string parametr ega bolsin,
+//  hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin.
+//   MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true.
+
+// @MITASK */
+
+function checkContent(xoji, boy) {
+  return [...xoji].sort().join("") === [...boy].sort().join("");
+}
+
+console.log(checkContent("mitgroup", "gmtiprou"));
+console.log("============");
+function checkContent1(eshmat, teshmat) {
+  return [...eshmat].sort().join("") === [...teshmat].sort().join("");
+}
+console.log(checkContent1("xojiakbar", "rabkaijox"));
+
+console.log("============");
+function checkContent(eshmat1, teshmat2) {
+  return [...eshmat1].sort().join("") === [...teshmat2].sort().join("");
+}
+console.log(
+  checkContent1("xojiakbar", "rabkaijoh"),
+); /* Xulosa bu yerdagi 2 ta string ichidagi harflar teng buls amisol uchun 2 ta x, 2 a va hokaza bulsa true
+chiqarakan moboda bittasi boshqacha bulib qolsa false chiqarakan */
