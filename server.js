dde4fcc3fb8982cefcbcb830a2cd43177410d759
Sam <yuldashevsamandar9@gmail.com>
2026-05-10 08:26:06 +0900

    fix: Port uzgardi

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongodb.connect(
 
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 3007;
       server.listen(PORT, function () {
         console.log(
           `The server running succesfully on port:${PORT},  http://localhost:${PORT} `,
